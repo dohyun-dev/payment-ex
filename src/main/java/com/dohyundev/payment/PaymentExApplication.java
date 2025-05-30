47d0b36e89fefb19da788da1ea857761c10d12d5
dohyundev0425 <kwondh0351@modaoutlet.co.kr>
2025-05-30 13:08:43 +0900

    backend project init

---
+++ src/main/java/com/dohyundev/payment/PaymentExApplication.java
@@ -0,0 +1,13 @@
+package com.dohyundev.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dohyundev/payment/PaymentExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dohyundev.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
